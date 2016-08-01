bcd79fac4904574bc54ccac08057e2fe584cc64e
Gennadiy Lebedintsev <darkzlodey@mail.ru>
2016-08-01 17:02:50 +0300

    Update EratosfenSieve.java

--- Practise/src/sieve/EratosfenSieve.java
+++ Practise/src/sieve/EratosfenSieve.java
@@ -2,47 +2,41 @@ package sieve;
 
 import java.util.Scanner;
 
-import static java.lang.System.out;
-
-import java.lang.Math;
-
 public class EratosfenSieve {
+    public static final int SIZE = 100_000_000;
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int size = (int) Math.pow(10, 6);
-        int array[] = new int[size];
+
         int leftBorder = 0, rightBorder = 0;
-        boolean flagArray[] = new boolean[size];
+        boolean flagArray[] = new boolean[SIZE];
 
         System.out.println("Input left and right border:");
 
-        while ((input.hasNextInt()) && (!input.equals("end"))) {
-            int answer1 = 0, answer2 = 0;
+        for (int i = 1; i < flagArray.length; i++) {
+            flagArray[i] = true;
+        }
+        flagArray[1] = false;
+        for (int i = 2; i * i < SIZE; i++) {
+            if (flagArray[i]) {
+                for (int j = i * i; j < SIZE; j += i) {
+                    flagArray[j] = false;
+                }
+            }
+        }
+
+        while (input.hasNextInt()) {
+            int answer = 0;
 
             leftBorder = input.nextInt();
             rightBorder = input.nextInt();
 
-            for (int i = 1; i < array.length; i++) {
-                array[i] = i;
-                flagArray[i] = true;
-            }
-            flagArray[1] = false;
-            for (int i = 2; i * i < size; i++) {
-                if (flagArray[i]) {
-                    for (int j = i * i; j < size; j += i) {
-                        flagArray[j] = false;
-                    }
-                }
-            }
 
-            for (int i = 0; i < size; i++) {
-                if ((flagArray[i]) && (i < leftBorder)) answer1++;
-                if ((flagArray[i]) && (i < rightBorder)) answer2++;
+            for (int i = leftBorder; i < rightBorder; i++) {
+                if (flagArray[i]) answer++;
             }
-            System.out.println("Answer is: " + Math.abs(answer2 - answer1));
+            System.out.println("Answer is: " + answer);
+
         }
-        if (input.equals("end")) System.exit(0);
-
-        System.out.println();
     }
 }
