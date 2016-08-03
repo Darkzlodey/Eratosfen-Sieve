003834e15dcd5d6a79c23d9866ad35bf2a2fd538
Gennadiy Lebedintsev <darkzlodey@mail.ru>
2016-08-03 17:41:35 +0300

    Create void createArrayOfPrimeNumbers to avoid every time computing arrayOfPrimeNumbers

--- Practise/src/sieve/AlgorithmOfEratosfenSieve.java
+++ Practise/src/sieve/AlgorithmOfEratosfenSieve.java
@@ -2,34 +2,24 @@ package sieve;
 
 public class AlgorithmOfEratosfenSieve {
     public static final int SIZE = 100_000_000;
+   public static boolean isPrimeNumber[] = new boolean[SIZE];
 
-    int firstNumber;
-    int secondNumber;
-
-    public AlgorithmOfEratosfenSieve(int rightBorder, int leftBorder) {
-        firstNumber = rightBorder;
-        secondNumber = leftBorder;
-    }
-
-    public static int countPrimeNumbers(int firstNumber, int lastNumber) {
-
-        boolean isPrimeNumber[] = new boolean[SIZE];
+    public static void  createArrayOfPrimeNumbers(){
         for (int i = 1; i < isPrimeNumber.length; i++) {
             isPrimeNumber[i] = true;
         }
-        isPrimeNumber[1] = false;
-
-
-        for (int i = 2; i * i < SIZE; i++) {
-            if (isPrimeNumber[i]) {
-                for (int j = i * i; j < SIZE; j += i) {
-                    isPrimeNumber[j] = false;
+        for (int everyNumberIndex = 2; everyNumberIndex * everyNumberIndex < SIZE; everyNumberIndex++) {
+            if (isPrimeNumber[everyNumberIndex]) {
+                for (int notPrimeNumberIndex = everyNumberIndex * everyNumberIndex; notPrimeNumberIndex < SIZE; notPrimeNumberIndex += everyNumberIndex) {
+                    isPrimeNumber[notPrimeNumberIndex] = false;
                 }
             }
         }
+    }
+    public static   int countPrimeNumbers(int firstNumber, int lastNumber) {
         int numberOfPrimeElementsInInterval = 0;
-        for (int i = firstNumber; i < lastNumber + 1; i++) {
-            if (isPrimeNumber[i]) {
+        for (int everyNumberIndex = firstNumber; everyNumberIndex < lastNumber + 1; everyNumberIndex++) {
+            if (isPrimeNumber[everyNumberIndex]) {
                 numberOfPrimeElementsInInterval++;
             }
         }
--- Practise/src/sieve/EratosfenSieve.java
+++ Practise/src/sieve/EratosfenSieve.java
@@ -3,20 +3,17 @@ package sieve;
 import java.util.Scanner;
 
 public class EratosfenSieve {
-
     public static void main(String[] args) {
-
-
         System.out.println("Input left and right border:");
-
         Scanner input = new Scanner(System.in);
 
         int firstNumber = 0, lastNumber = 0;
 
+        AlgorithmOfEratosfenSieve.createArrayOfPrimeNumbers();
+
         while (input.hasNextInt()) {
             firstNumber = input.nextInt();
             lastNumber = input.nextInt();
-
             System.out.println("Answer is: " + AlgorithmOfEratosfenSieve.countPrimeNumbers(firstNumber, lastNumber));
         }
     }
